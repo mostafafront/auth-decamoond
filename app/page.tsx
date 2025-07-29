0d096ab3c7219bad4accdc1998fc5603739c6d4b
mostafa hosseini <mostafafront1@gmail.com>
2025-07-30 01:36:59 +0330

    create sass file for styling auth page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import styles from "@/styles/pages/auth.module.scss";
 
-export default function Home() {
+export default function Auth() {
   return (
-    
+    <main>
+      <div className={styles.mainSectoin}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore pariatur modi placeat cumque optio ipsa ex praesentium, quas consequatur eligendi ea et maiores eum consequuntur quaerat ipsam corrupti! Saepe, excepturi?
+      </div>
+    </main>
   );
 }
